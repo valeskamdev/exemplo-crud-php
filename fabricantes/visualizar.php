9422f0f2862c385a09ea8db36ad7c639586da1b8
Valeska Marques <valeskkitty@gmail.com>
2023-08-24 09:03:48 -0300

    Initial commit

---
+++ fabricantes/visualizar.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Fabricantes - Visualização</title>
+</head>
+<body>
+  <h1>Fabricantes | SELECT <a href="../index.php">Home</a></h1>
+  <hr>
+  <p><a href="inserir.php">Inserir novo fabricante</a></p>
+  <h2>Lendo e carregando todos os fabricantes</h2>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Sistemas Vendas - CRUD PHP e SQL</h1>
+    <hr>
+
+  <p>Operações de <b>inserção</b>
+  <b>Leitura</b>, <b>atualização</b>, <b>execução</b> de dados </p>
+  <hr>
+
+  <h2>Escoçha o que deseja gerenciar</h2>
+    <ul>
+      <li><a href="fabricantes/visualizar.php">Fabricantes</a></li>
+      <li><a href="">Produtos</a></li>
+    </ul>
+
+</body>
+</html>
